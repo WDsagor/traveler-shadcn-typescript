5855c7e88cf10d4763d8eca64b3cc66d759497ee
Wdsagor <sagarwpress@gmail.com>
2024-09-03 18:28:54 +0600

    added active button ui interface

--- app/news/TravelajaArticles.tsx
+++ app/news/TravelajaArticles.tsx
@@ -1,14 +1,19 @@
+
+"use client"
 import { Button } from '@/components/ui/button';
-import React from 'react';
+import React, { useState } from 'react';
+import { buttonVariants } from "@/components/ui/button"
 
 const TravelajaArticles = () => {
+    const [index, setIndex] = useState(0)
+    console.log(index);
     return (
         <div>
             <h1 className='text-4xl font-normal text-center '>Travelaja Articles</h1>
             <p className='text-center py-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam</p>
             <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-x-10 gap-y-5 w-full xl:w-2/3 mx-auto py-5 '>
                 {["Adventure Travel", "Beach", "Explore World", "Family Holidays", "Art and culture"].map((name, i) => {
-                    return <Button size={"lg"} className='text-lg text-white' key={i}>{name}</Button>
+                    return <Button onClick={() => setIndex(i)} size={"lg"} className={`${index === i ? "text-lg font-medium text-white" : "bg-transparent text-lg  hover:text-white font-medium "}" `} key={i}>{name}</Button>
                 })}
             </div>
         </div>
